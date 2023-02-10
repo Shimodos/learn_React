05faa59dc202350f16a24ab87377a542645ac40e
Alekssey Demchenko <ad@pesochin.net>
2023-02-10 20:12:58 +0200

    Strict Mode и React 18+

--- 02/src/index.js
+++ 02/src/index.js
@@ -1,21 +1,12 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
-
-const text = 'Hello';
-const elem = (
-  <div>
-    <h2>Text: {text}</h2>
-    <input type="text" />
-    <button tabIndex={0}>Click</button>
-  </div>
-);
+import App from './App';
+// import { Header } from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>,
-  elem
+    <StrictMode>
+        <App />
+    </StrictMode>
 );
