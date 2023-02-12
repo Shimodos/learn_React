cfcf9bc2fdbc658280381995c43ee314ddf0185d
Alekssey Demchenko <ad@pesochin.net>
2023-02-12 12:25:08 +0200

    Employees modul_01-Component properties

--- 02/src/App.js
+++ 02/src/App.js
@@ -13,31 +13,56 @@ const Header = () => {
 //     return <input placeholder={holder} type="text" style={styledField} />;
 // };
 
-class Field extends Component {
-    render() {
-        const holder = 'Enter here';
-        const styledField = {
-            width: '300px',
-        };
-        return <input placeholder={holder} type="text" style={styledField} />;
-    }
-}
+// class Field extends Component {
+//     render() {
+//         const holder = 'Enter here';
+//         const styledField = {
+//             width: '300px',
+//         };
+//         return <input placeholder={holder} type="text" style={styledField} />;
+//     }
+// }
 
-function Btn() {
-    const text = 'Log in';
-    const logged = false;
+// function Btn() {
+//     const text = 'Log in';
+//     const logged = false;
 
-    return <button>{logged ? 'Enter' : text}</button>;
+//     return <button>{logged ? 'Enter' : text}</button>;
+// }
+
+function WhoAmI({ name, surname, link }) {
+    return (
+        <div>
+            <h1>
+                My name {name()} is, surname - {surname}
+            </h1>
+            <a href={link}>My profil</a>
+        </div>
+    );
 }
 
 function App() {
     return (
         <div className="App">
-            <StrictMode>
+            <WhoAmI
+                name={() => {
+                    return 'John';
+                }}
+                surname="Smith"
+                link="facebook.com"
+            />
+            <WhoAmI
+                name={() => {
+                    return 'Alex';
+                }}
+                surname="Demchenko"
+                link="twitter.com"
+            />
+            {/* <StrictMode>
                 <Header />
-            </StrictMode>
-            <Field />
-            <Btn />
+            </StrictMode> */}
+            {/* <Field /> */}
+            {/* <Btn /> */}
         </div>
     );
 }
