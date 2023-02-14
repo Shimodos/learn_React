b0189a4f81fca29e9745ddd3933347ea524fb577
Alekssey Demchenko <ad@pesochin.net>
2023-02-14 21:13:11 +0200

    Employees modul_01-The practice of states on the project

--- employees_template/src/components/app/app.js
+++ employees_template/src/components/app/app.js
@@ -7,25 +7,25 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './app.css';
 
 function App() {
-    const data = [
-        { name: 'John C.', salary: 800, increase: false, id: 1 },
-        { name: 'Alex M.', salary: 3000, increase: true, id: 2 },
-        { name: 'Carl V.', salary: 5000, increase: false, id: 3 },
-    ];
+  const data = [
+    { name: 'John C.', salary: 800, increase: false, id: 1 },
+    { name: 'Alex M.', salary: 3000, increase: true, id: 2 },
+    { name: 'Carl V.', salary: 5000, increase: false, id: 3 },
+  ];
 
-    return (
-        <div className="app">
-            <AppInfo />
+  return (
+    <div className="app">
+      <AppInfo />
 
-            <div className="search-panel">
-                <SearchPanel />
-                <AppFilter />
-            </div>
+      <div className="search-panel">
+        <SearchPanel />
+        <AppFilter />
+      </div>
 
-            <EmployeesList data={data} />
-            <EmployeesAddForm />
-        </div>
-    );
+      <EmployeesList data={data} />
+      <EmployeesAddForm />
+    </div>
+  );
 }
 
 export default App;
