6d8bb38661409ff0259decc75545e82bce8548aa
Alekssey Demchenko <ad@pesochin.net>
2023-02-15 23:29:08 +0200

    Working with Forms, Managed and Unmanaged Components_fix

--- employees_template/src/components/app/app.js
+++ employees_template/src/components/app/app.js
@@ -22,7 +22,7 @@ function App() {
         <AppFilter />
       </div>
 
-      <EmployeesList data={data} />
+      <EmployeesList data={data} onDelete={(id) => console.log(id)} />
       <EmployeesAddForm />
     </div>
   );
