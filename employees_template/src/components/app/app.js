d8b4130a4e74d181dee09e807a7b5d83738c13cc
Alekssey Demchenko <ad@pesochin.net>
2023-02-17 21:04:13 +0200

    State Immutability and Custom Events_add_items

--- employees_template/src/components/app/app.js
+++ employees_template/src/components/app/app.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -17,12 +18,28 @@ class App extends Component {
         { name: 'Carl V.', salary: 5000, increase: false, id: 3 },
       ],
     };
+    this.maxId = 4;
   }
 
   delateItem = (id) => {
     this.setState(({ data }) => {
       return {
         data: data.filter((item) => item.id !== id),
+      };
+    });
+  };
+
+  addItem = (name, salary) => {
+    const newItem = {
+      name,
+      salary,
+      increase: false,
+      id: this.maxId++,
+    };
+    this.setState(({ data }) => {
+      const newArr = [...data, newItem];
+      return {
+        data: newArr,
       };
     });
   };
@@ -38,7 +55,7 @@ class App extends Component {
         </div>
 
         <EmployeesList data={this.state.data} onDelete={this.delateItem} />
-        <EmployeesAddForm />
+        <EmployeesAddForm onAdd={this.addItem} />
       </div>
     );
   }
