1f40ed72b847c3ccbe954fe64e045347fc287844
Alekssey Demchenko <ad@pesochin.net>
2023-02-20 18:53:29 +0200

    Implementing search and filters

--- employees_template/src/components/app/app.js
+++ employees_template/src/components/app/app.js
@@ -17,6 +17,8 @@ class App extends Component {
         { name: 'Alex M.', salary: 3000, increase: true, rise: false, id: 2 },
         { name: 'Carl V.', salary: 5000, increase: false, rise: false, id: 3 },
       ],
+      term: '',
+      filter: 'all',
     };
     this.maxId = 4;
   }
@@ -45,28 +47,6 @@ class App extends Component {
     });
   };
 
-  // onToggleIncrease = (id) => {
-  //   this.setState(({ data }) => ({
-  //     data: data.map((item) => {
-  //       if (item.id === id) {
-  //         return { ...item, increase: !item.increase };
-  //       }
-  //       return item;
-  //     }),
-  //   }));
-  // };
-
-  // onToggleRise = (id) => {
-  //   this.setState(({ data }) => ({
-  //     data: data.map((item) => {
-  //       if (item.id === id) {
-  //         return { ...item, rise: !item.rise };
-  //       }
-  //       return item;
-  //     }),
-  //   }));
-  // };
-
   onToggleProp = (id, prop) => {
     this.setState(({ data }) => ({
       data: data.map((item) => {
@@ -78,20 +58,51 @@ class App extends Component {
     }));
   };
 
+  searchEmp = (items, term) => {
+    if (term.length === 0) {
+      return items;
+    }
+    return items.filter((item) => {
+      return item.name.indexOf(term) > -1;
+    });
+  };
+
+  onUpdateSearch = (term) => {
+    this.setState({ term });
+  };
+
+  filterPost = (items, filter) => {
+    switch (filter) {
+      case 'rise':
+        return items.filter((item) => item.rise);
+      case 'moreThen1000':
+        return items.filter((item) => item.salary > 1000);
+      default:
+        return items;
+    }
+  };
+
+  onFilterSelect = (filter) => {
+    this.setState({ filter });
+  };
+
   render() {
+    const { data, term, filter } = this.state;
     const employees = this.state.data.length;
     const increased = this.state.data.filter((item) => item.increase).length;
+    const visibleData = this.filterPost(this.searchEmp(data, term), filter);
+
     return (
       <div className="app">
         <AppInfo employees={employees} increased={increased} />
 
         <div className="search-panel">
-          <SearchPanel />
-          <AppFilter />
+          <SearchPanel onUpdateSearch={this.onUpdateSearch} />
+          <AppFilter filter={filter} onFilterSelect={this.onFilterSelect} />
         </div>
 
         <EmployeesList
-          data={this.state.data}
+          data={visibleData}
           onDelete={this.delateItem}
           onToggleProp={this.onToggleProp}
         />
--- employees_template/src/components/search-panel/search-panel.js
+++ employees_template/src/components/search-panel/search-panel.js
@@ -1,9 +1,31 @@
+import { Component } from 'react';
 import './search-panel.css';
 
-const SearchPanel = () => {
+class SearchPanel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: '',
+    };
+  }
+
+  onUpdateSearch = (e) => {
+    const term = e.target.value;
+    this.setState({ term });
+    this.props.onUpdateSearch(term);
+  };
+
+  render() {
     return (
-        <input type="text" className="form-control cearch-input" placeholder="Найти сотрудник" />
+      <input
+        type="text"
+        className="form-control cearch-input"
+        placeholder="Найти сотрудник"
+        value={this.state.term}
+        onChange={this.onUpdateSearch}
+      />
     );
-};
+  }
+}
 
 export default SearchPanel;
