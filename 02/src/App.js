72ae0ad97cb036caca23da256a07fa3fccd8f3e7
Alekssey Demchenko <ad@pesochin.net>
2023-02-21 22:27:28 +0200

    Styles in React. Styled Components

--- 02/src/App.js
+++ 02/src/App.js
@@ -1,9 +1,11 @@
 import { Component } from 'react';
+import styled from 'styled-components';
+
 import './App.css';
 
-const Header = () => {
-    return <h2>Hello worls!</h2>;
-};
+// const Header = () => {
+//   return <h2>Hello worls!</h2>;
+// };
 
 // const Field = () => {
 //     const holder = 'Enter here';
@@ -30,67 +32,95 @@ const Header = () => {
 //     return <button>{logged ? 'Enter' : text}</button>;
 // }
 
+const EmpItem = styled.div`
+  padding: 20px;
+  margin-bottom: 15px;
+  border-radius: 5px;
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
+  a {
+    display: block;
+    margin: 10px 0 10px 0;
+    color: ${(props) => (props.active ? 'orange' : 'black')};
+  }
+  input {
+    display: block;
+    margin-top: 10px;
+  }
+`;
+
+const Header = styled.h2`
+  font-size: 22px;
+`;
+
+export const Button = styled.button`
+  display: block;
+  padding: 5px 5px;
+  background-color: gold;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
+`;
+
 class WhoAmI extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            years: 27,
-            Text: '+++',
-            position: '',
-        };
-    }
-
-    nextYear = () => {
-        console.log('++');
-        this.setState((state) => ({
-            years: state.years + 1,
-        }));
+  constructor(props) {
+    super(props);
+    this.state = {
+      years: 27,
+      Text: '+++',
+      position: '',
     };
+  }
 
-    commitInputChanges = (e, color) => {
-        console.log(color);
-        this.setState({
-            position: e.target.value,
-        });
-    };
+  nextYear = () => {
+    console.log('++');
+    this.setState((state) => ({
+      years: state.years + 1,
+    }));
+  };
 
-    render() {
-        const { name, surname, link } = this.props;
-        const { position, years } = this.state;
-        return (
-            <div>
-                <button onClick={this.nextYear}>{this.state.Text}</button>
-                <h1>
-                    My name {name} is, surname - {surname}, age - {years},
-                    position - {position}
-                </h1>
-                <a href={link}>My profil</a>
-                <forma>
-                    <span>Введітедолжность</span>
-                    <input
-                        type="text"
-                        onChange={(e) =>
-                            this.commitInputChanges(e, 'some color')
-                        }
-                    />
-                </forma>
-            </div>
-        );
-    }
+  commitInputChanges = (e, color) => {
+    console.log(color);
+    this.setState({
+      position: e.target.value,
+    });
+  };
+
+  render() {
+    const { name, surname, link } = this.props;
+    const { position, years } = this.state;
+
+    return (
+      <EmpItem active>
+        <Button onClick={this.nextYear}>{this.state.Text}</Button>
+        <Header>
+          My name {name} is, surname - {surname}, age - {years}, position - {position}
+        </Header>
+        <a href={link}>My profil</a>
+        <forma>
+          <span>Введіте должность</span>
+          <input type="text" onChange={(e) => this.commitInputChanges(e, 'some color')} />
+        </forma>
+      </EmpItem>
+    );
+  }
 }
 
+const Wrapper = styled.div`
+  width: 600px;
+  margin: 80px auto 0 auto;
+`;
+
 function App() {
-    return (
-        <div className="App">
-            <WhoAmI name="John" surname="Smith" link="facebook.com" />
-            <WhoAmI name="Alex" surname="Demchenko" link="twitter.com" />
-            {/* <StrictMode>
+  return (
+    <Wrapper className="App">
+      <WhoAmI name="John" surname="Smith" link="facebook.com" />
+      <WhoAmI name="Alex" surname="Demchenko" link="twitter.com" />
+      {/* <StrictMode>
                 <Header />
             </StrictMode> */}
-            {/* <Field /> */}
-            {/* <Btn /> */}
-        </div>
-    );
+      {/* <Field /> */}
+      {/* <Btn /> */}
+    </Wrapper>
+  );
 }
 
 export { Header };
--- 02/src/index.js
+++ 02/src/index.js
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 // import { Header } from './App';
+import { Button } from './App';
+import styled from 'styled-components';
+
+const BigButton = styled(Button)`
+  margin: 0 auto;
+  width: 245px;
+  text-align: center;
+  /* position: relative; */
+`;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <StrictMode>
-        <App />
-    </StrictMode>
+  <StrictMode>
+    <App />
+    <BigButton as="a">send order</BigButton>
+  </StrictMode>
 );
