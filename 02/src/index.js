c13e541e97a66961bc331a3843a6260556ae49d8
Alekssey Demchenko <ad@pesochin.net>
2023-02-22 20:35:16 +0200

    Styles in React. Ready component libraries with styles

--- 02/src/index.js
+++ 02/src/index.js
@@ -4,7 +4,10 @@ import './index.css';
 import App from './App';
 // import { Header } from './App';
 import { Button } from './App';
+import BootstrapTest from './BootstrtapTest';
 import styled from 'styled-components';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const BigButton = styled(Button)`
   margin: 0 auto;
@@ -18,5 +21,6 @@ root.render(
   <StrictMode>
     <App />
     <BigButton as="a">send order</BigButton>
+    <BootstrapTest />
   </StrictMode>
 );
