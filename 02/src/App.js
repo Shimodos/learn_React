6dd399223587b8d56f47e1fa6d8ca20e902ee011
Alekssey Demchenko <ad@pesochin.net>
2023-03-14 21:41:03 +0200

    Specialization and inheritance

--- 02/src/App.js
+++ 02/src/App.js
@@ -120,9 +120,20 @@ const DynamicGreating = (props) => {
   );
 };
 
+const HelloGreating = () => {
+  return (
+    <div style={{ width: '600px', margin: '0 auto' }}>
+      <DynamicGreating color={'primary'}>
+        <h2>This well was hard</h2>
+      </DynamicGreating>
+    </div>
+  );
+};
+
 function App() {
   return (
     <Wrapper className="App">
+      <HelloGreating />
       <BootstrapTest
         left={
           <DynamicGreating color={'primary'}>
