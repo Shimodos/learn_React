19e3af98d754de526bc6a1cfbd62f9e68dfe8a93
Alekssey Demchenko <ad@pesochin.net>
2023-03-16 18:34:14 +0200

    Portals

--- 02/src/App.js
+++ 02/src/App.js
@@ -1,34 +1,36 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import { Container } from 'react-bootstrap';
 import './App.css';
 
 class Form extends Component {
-  // myRef = React.createRef();
-
-  // componentDidMount() {
-  //   this.myRef.current.focus();
-  // }
-
-  setInputRef = (elem) => {
-    this.myRef = elem;
+  state = {
+    advOpen: false,
   };
 
-  focusFirstTI = () => {
-    if (this.myRef) {
-      this.myRef.focus();
-    }
+  hendleClick = () => {
+    this.setState(({ advOpen }) => ({
+      advOpen: !advOpen,
+    }));
   };
+
+  componentDidMount() {
+    setTimeout(this.hendleClick, 3000);
+  }
 
   render() {
     return (
       <Container>
-        <form className="w-50 border mt-5 p-3 m-auto">
+        <form
+          onClick={this.hendleClick}
+          className="w-50 border mt-5 p-3 m-auto"
+          style={{ overflow: 'hidden', position: 'relative' }}
+        >
           <div className="mb-3">
             <label htmlFor="exampleFormControlInput1" className="form-label">
               Email address
             </label>
             <input
-              ref={this.setInputRef}
               type="email"
               className="form-control"
               id="exampleFormControlInput1"
@@ -39,18 +41,42 @@ class Form extends Component {
             <label htmlFor="exampleFormControlTextarea1" className="form-label">
               Example textarea
             </label>
-            <textarea
-              onClick={this.focusFirstTI}
-              className="form-control"
-              id="exampleFormControlTextarea1"
-              rows="3"
-            ></textarea>
+            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
           </div>
+          {this.state.advOpen ? (
+            <Portal>
+              <Msg />
+            </Portal>
+          ) : null}
         </form>
       </Container>
     );
   }
 }
+
+const Portal = (props) => {
+  const node = document.createElement('div');
+  document.body.appendChild(node);
+
+  return ReactDOM.createPortal(props.children, node);
+};
+
+const Msg = () => {
+  return (
+    <div
+      style={{
+        width: '500px',
+        height: '150px',
+        backgroundColor: 'red',
+        position: 'absolute',
+        right: '0',
+        bottom: '0',
+      }}
+    >
+      Hello
+    </div>
+  );
+};
 
 function App() {
   return <Form />;
