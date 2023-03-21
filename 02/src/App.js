fcbe781be7c8449e2ef1d437a43fbeb2768cfae6
Alekssey Demchenko <ad@pesochin.net>
2023-03-21 19:05:06 +0200

    useRef

--- 02/src/App.js
+++ 02/src/App.js
@@ -1,80 +1,44 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import { useRef, useState, useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 import './App.css';
 
-class Form extends Component {
-  state = {
-    advOpen: false,
-  };
+const Form = () => {
+  const [text, setText] = useState('');
 
-  hendleClick = () => {
-    this.setState(({ advOpen }) => ({
-      advOpen: !advOpen,
-    }));
-  };
+  const myRef = useRef(1);
 
-  componentDidMount() {
-    setTimeout(this.hendleClick, 3000);
-  }
+  useEffect(() => {
+    myRef.current = text;
+  });
 
-  render() {
-    return (
-      <Container>
-        <form
-          onClick={this.hendleClick}
-          className="w-50 border mt-5 p-3 m-auto"
-          style={{ overflow: 'hidden', position: 'relative' }}
-        >
-          <div className="mb-3">
-            <label htmlFor="exampleFormControlInput1" className="form-label">
-              Email address
-            </label>
-            <input
-              type="email"
-              className="form-control"
-              id="exampleFormControlInput1"
-              placeholder="name@example.com"
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="exampleFormControlTextarea1" className="form-label">
-              Example textarea
-            </label>
-            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-          </div>
-          {this.state.advOpen ? (
-            <Portal>
-              <Msg />
-            </Portal>
-          ) : null}
-        </form>
-      </Container>
-    );
-  }
-}
-
-const Portal = (props) => {
-  const node = document.createElement('div');
-  document.body.appendChild(node);
-
-  return ReactDOM.createPortal(props.children, node);
-};
-
-const Msg = () => {
   return (
-    <div
-      style={{
-        width: '500px',
-        height: '150px',
-        backgroundColor: 'red',
-        position: 'absolute',
-        right: '0',
-        bottom: '0',
-      }}
-    >
-      Hello
-    </div>
+    <Container>
+      <form className="w-50 border mt-5 p-3 m-auto">
+        <div className="mb-3">
+          <label htmlFor="exampleFormControlInput1" className="form-label">
+            Email address
+          </label>
+          <input
+            onChange={(e) => setText(e.target.value)}
+            type="email"
+            className="form-control"
+            id="exampleFormControlInput1"
+            placeholder="name@example.com"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleFormControlTextarea1" className="form-label">
+            Example textarea
+          </label>
+          <textarea
+            value={myRef.current}
+            className="form-control"
+            id="exampleFormControlTextarea1"
+            rows="3"
+          ></textarea>
+        </div>
+      </form>
+    </Container>
   );
 };
 
