c50c945684e88766c7cf0e1e75d852cf9abae6df
Alekssey Demchenko <ad@pesochin.net>
2023-03-22 18:21:06 +0200

    Creando tus propios ganchos

--- 02/src/App.js
+++ 02/src/App.js
@@ -1,27 +1,44 @@
-import { useRef, useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 import './App.css';
 
+function useInputWithWalidate(initialValue) {
+  const [value, setValue] = useState(initialValue);
+
+  const onChanged = (event) => {
+    setValue(event.target.value);
+  };
+
+  const validateInput = () => {
+    return value.search(/\d/) >= 0;
+  };
+
+  return { value, onChanged, validateInput };
+}
+
 const Form = () => {
-  const [text, setText] = useState('');
+  const input = useInputWithWalidate('');
+  const textArea = useInputWithWalidate('');
 
-  const myRef = useRef(1);
-
-  useEffect(() => {
-    myRef.current = text;
-  });
-
+  const color = input.validateInput() ? 'text-danger' : null;
   return (
     <Container>
       <form className="w-50 border mt-5 p-3 m-auto">
         <div className="mb-3">
-          <label htmlFor="exampleFormControlInput1" className="form-label">
+          <input
+            value={`${input.value} / ${textArea.value}`}
+            type="text"
+            className="form-control"
+            readOnly
+          />
+          <label htmlFor="exampleFormControlInput1" className="form-label mt-3">
             Email address
           </label>
           <input
-            onChange={(e) => setText(e.target.value)}
+            onChange={input.onChanged}
             type="email"
-            className="form-control"
+            value={input.value}
+            className={`form-control ${color}`}
             id="exampleFormControlInput1"
             placeholder="name@example.com"
           />
@@ -31,7 +48,8 @@ const Form = () => {
             Example textarea
           </label>
           <textarea
-            value={myRef.current}
+            onChange={textArea.onChanged}
+            value={textArea.value}
             className="form-control"
             id="exampleFormControlTextarea1"
             rows="3"
