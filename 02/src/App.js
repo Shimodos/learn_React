d3c4fdac4bdb1be408e86e2518f787bde2c45e6f
Alekssey Demchenko <ad@pesochin.net>
2023-04-03 18:56:47 +0300

    React.memo, Pure Component and Application Speed Optimization

--- 02/src/App.js
+++ 02/src/App.js
@@ -1,39 +1,108 @@
-import data from './data';
-import { useState, useMemo, useTransition } from 'react';
+import { useState, memo, Component } from 'react';
+import { Container } from 'react-bootstrap';
+import './App.css';
+
+class Form extends Component {
+  shouldComponentUpdate(nextProps) {
+    if (this.props.mail.name === nextProps.mail.name) {
+      return false;
+    }
+    return true;
+  }
+
+  render() {
+    console.log('render');
+    return (
+      <Container>
+        <form className="w-50 border mt-5 p-3 m-auto">
+          <div className="mb-3">
+            <label htmlFor="exampleFormControlInput1" className="form-label mt-3">
+              Email address
+            </label>
+            <input
+              value={this.props.mail.name}
+              type="email"
+              className="form-control"
+              id="exampleFormControlInput1"
+              placeholder="name@example.com"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleFormControlTextarea1" className="form-label">
+              Example textarea
+            </label>
+            <textarea
+              value={this.props.text}
+              className="form-control"
+              id="exampleFormControlTextarea1"
+              rows="3"
+            ></textarea>
+          </div>
+        </form>
+      </Container>
+    );
+  }
+}
+// function propsCompare(prevProps, nextProps) {
+//   return prevProps.mail.name === nextProps.mail.name && prevProps.text === nextProps.text;
+// }
+
+// const Form = memo((props) => {
+//   console.log('render');
+//   return (
+//     <Container>
+//       <form className="w-50 border mt-5 p-3 m-auto">
+//         <div className="mb-3">
+//           <label htmlFor="exampleFormControlInput1" className="form-label mt-3">
+//             Email address
+//           </label>
+//           <input
+//             value={props.mail.name}
+//             type="email"
+//             className="form-control"
+//             id="exampleFormControlInput1"
+//             placeholder="name@example.com"
+//           />
+//         </div>
+//         <div className="mb-3">
+//           <label htmlFor="exampleFormControlTextarea1" className="form-label">
+//             Example textarea
+//           </label>
+//           <textarea
+//             value={props.text}
+//             className="form-control"
+//             id="exampleFormControlTextarea1"
+//             rows="3"
+//           ></textarea>
+//         </div>
+//       </form>
+//     </Container>
+//   );
+// }, propsCompare);
 
 function App() {
-  const [text, setText] = useState('');
-  const [posts, setPosts] = useState(data);
-  // const defferedValue = useDeferredValue(text);
-  const [isPanding, startTransition] = useTransition();
-
-  const filteredPosts = useMemo(() => {
-    return posts.filter((item) => item.name.toLowerCase().includes(text));
-  }, [text]);
-
-  const onValueChange = (e) => {
-    startTransition(() => {
-      setText(e.target.value);
-    });
-  };
+  const [data, setData] = useState({
+    mail: {
+      name: 'example@example.com',
+    },
+    text: 'some text',
+  });
 
   return (
     <>
-      <input value={text} type="text" onChange={onValueChange} />
-
-      <hr />
-
-      <div>
-        {isPanding ? (
-          <h4>Loading...</h4>
-        ) : (
-          filteredPosts.map((post) => (
-            <div key={post._id}>
-              <h4>{post.name}</h4>
-            </div>
-          ))
-        )}
-      </div>
+      <Form mail={data.mail} text={data.text} />
+      <button
+        onClick={() =>
+          setData({
+            mail: {
+              name: 'xample@example.com',
+            },
+            text: 'some text',
+          })
+        }
+      >
+        Click me
+      </button>
     </>
   );
 }
