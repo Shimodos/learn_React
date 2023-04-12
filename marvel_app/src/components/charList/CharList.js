33ed5c0e2c7f87f9b3e448dc6390343bd87729ce
Alekssey Demchenko <ad@pesochin.net>
2023-04-12 18:44:28 +0300

    We analyze the errors of third-party libraries and the problem with focus

--- marvel_app/src/components/charList/CharList.js
+++ marvel_app/src/components/charList/CharList.js
@@ -1,9 +1,8 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, useMemo } from 'react';
 import PropTypes from 'prop-types';
 
 import useMarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/Spinner';
-import ErrorMessage from '../errorMessage/ErrorMessage';
 
 import './charList.scss';
 
@@ -11,16 +10,11 @@ const setContent = (process, Component, newItemLoading) => {
   switch (process) {
     case 'waiting':
       return <Spinner />;
-      break;
     case 'loading':
       return newItemLoading ? <Component /> : <Spinner />;
-      break;
     case 'confirmed':
       return <Component />;
-      break;
     case 'error':
-      return <ErrorMessage />;
-      break;
     default:
       throw new Error('Unexpected process state');
   }
@@ -32,10 +26,11 @@ const CharList = (props) => {
   const [offset, setOffset] = useState(210);
   const [charEnded, setCharEnded] = useState(false);
 
-  const { loading, error, getAllCharacters, process, setProcess } = useMarvelService();
+  const { getAllCharacters, process, setProcess } = useMarvelService();
 
   useEffect(() => {
     onRequest(offset, true);
+    // eslint-disable-next-line
   }, []);
 
   const onRequest = (offset, initial) => {
@@ -56,8 +51,6 @@ const CharList = (props) => {
     setOffset((offset) => offset + 9);
     setCharEnded((charEnded) => ended);
   };
-
-  console.log('charlist');
 
   const itemRefs = useRef([]);
 
@@ -111,9 +104,14 @@ const CharList = (props) => {
     return <ul className="char__grid">{items}</ul>;
   }
 
+  const elements = useMemo(() => {
+    return setContent(process, () => renderItems(charList), newItemLoading);
+    // eslint-disable-next-line
+  }, [process]);
+
   return (
     <div className="char__list">
-      {setContent(process, () => renderItems(charList), newItemLoading)}
+      {elements}
       <button
         className="button button__main button__long"
         disabled={newItemLoading}
--- marvel_app/src/components/pages/SinglePage.js
+++ marvel_app/src/components/pages/SinglePage.js
@@ -16,6 +16,7 @@ const SinglePage = ({ Component, dataType }) => {
 
   useEffect(() => {
     updateData();
+    // eslint-disable-next-line
   }, [id]);
 
   const updateData = () => {
@@ -31,6 +32,9 @@ const SinglePage = ({ Component, dataType }) => {
         getCharacter(id)
           .then(onDataLoaded)
           .then(() => setProcess('confirmed'));
+        break;
+      default:
+      // do nothing
     }
   };
 
