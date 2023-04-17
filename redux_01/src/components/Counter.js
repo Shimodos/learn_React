df97b9e406acfc401ff6e5e2b1d282204ebd6236
Alekssey Demchenko <ad@pesochin.net>
2023-04-17 18:34:00 +0300

    Connecting React and Redux with Hooks

--- redux_01/src/components/Counter.js
+++ redux_01/src/components/Counter.js
@@ -1,6 +1,8 @@
-import { Component } from 'react';
-import { connect } from 'react-redux';
-import * as action from '../action';
+// import { Component } from 'react';
+// import { connect } from 'react-redux';
+import { inc, dec, rnd } from '../action';
+
+import { useSelector, useDispatch } from 'react-redux';
 // import { bindActionCreators } from 'redux';
 
 // const Counter = ({ counter, inc, dec, rnd }) => {
@@ -20,34 +22,35 @@ import * as action from '../action';
 //   );
 // };
 
-class Counter extends Component {
-  render() {
-    const { counter, inc, dec, rnd } = this.props;
-    return (
-      <div classNmae="jumbotron">
-        <h1>{counter}</h1>
-        <button onClick={dec} className="btn btn-primary">
-          DEC
-        </button>
-        <button onClick={inc} className="btn btn-primary">
-          INC
-        </button>
-        <button onClick={rnd} className="btn btn-primary">
-          RND
-        </button>
-      </div>
-    );
-  }
-}
-
-const mapStateToProps = (state) => {
-  return {
-    counter: state.value,
-  };
+const Counter = () => {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  return (
+    <div classNmae="jumbotron">
+      <h1>{counter}</h1>
+      <button onClick={() => dispatch(dec())} className="btn btn-primary">
+        DEC
+      </button>
+      <button onClick={() => dispatch(inc())} className="btn btn-primary">
+        INC
+      </button>
+      <button onClick={() => dispatch(rnd())} className="btn btn-primary">
+        RND
+      </button>
+    </div>
+  );
 };
 
-// const mapDispatchToProps = (dispatch) => {
-//   return bindActionCreators(action, dispatch);
+// const mapStateToProps = (state) => {
+//   return {
+//     counter: state.value,
+//   };
 // };
 
-export default connect(mapStateToProps, action)(Counter);
+// // const mapDispatchToProps = (dispatch) => {
+// //   return bindActionCreators(action, dispatch);
+// // };
+
+// export default connect(mapStateToProps, action)(Counter);
+
+export default Counter;
