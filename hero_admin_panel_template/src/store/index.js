c91a4024cdfaea7a62e356806a79ce5d5472689d
Alekssey Demchenko <ad@pesochin.net>
2023-04-19 20:04:08 +0300

    Store enhancers

--- hero_admin_panel_template/src/store/index.js
+++ hero_admin_panel_template/src/store/index.js
@@ -1,11 +1,28 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, compose } from 'redux';
 
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filrters';
 
+const enhancer =
+  (createStore) =>
+  (...args) => {
+    const store = createStore(...args);
+
+    const oldDispatch = store.dispatch;
+    store.dispatch = (actions) => {
+      if (typeof actions === 'string') {
+        return oldDispatch({
+          type: actions,
+        });
+      }
+      return oldDispatch(actions);
+    };
+    return store;
+  };
+
 const store = createStore(
   combineReducers({ heroes, filters }),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  compose(enhancer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 );
 
 export default store;
