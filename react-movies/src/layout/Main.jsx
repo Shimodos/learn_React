a4c900a817db492bf9398c97cdadb5a1cdf5f1de
Alekssey Demchenko <ad@pesochin.net>
2023-05-13 15:24:44 +0300

    movies app add card movie

--- react-movies/src/layout/Main.jsx
+++ react-movies/src/layout/Main.jsx
@@ -1,5 +1,26 @@
-function Main() {
-  return <main className="container content">Hello from My Movies App</main>;
+import React from 'react';
+import { Movies } from '../components/Movies';
+
+class Main extends React.Component {
+  state = {
+    movies: [],
+  };
+
+  componentDidMount() {
+    fetch('http://www.omdbapi.com/?apikey=c2cd2c6d&s=matrix')
+      .then((response) => response.json())
+      .then((data) => this.setState({ movies: data.Search }));
+  }
+
+  render() {
+    const { movies } = this.state;
+
+    return (
+      <main className="container content">
+        {movies.length ? <Movies movies={this.state.movies} /> : <h3>Loading...</h3>}
+      </main>
+    );
+  }
 }
 
 export { Main };
