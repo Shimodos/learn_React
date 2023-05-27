8a44b7ef678c25e113958f6e5eca2de89a44e7de
Alekssey Demchenko <ad@pesochin.net>
2023-05-27 19:51:32 +0300

    React_Shop app Card_style

--- react-shop/src/components/GoodsItem.jsx
+++ react-shop/src/components/GoodsItem.jsx
@@ -13,14 +13,16 @@ function GoodsItem(props) {
     <div className="card" id={id}>
       <div className="card-image">
         <img src={background} alt={name} />
-        <span className="card-title">{name}</span>
       </div>
       <div className="card-content">
+        <span className="card-title">{name}</span>
         <p>{description}</p>
       </div>
       <div className="card-action">
         <button className="btn">Buy</button>
-        <span className="right">{price}</span>
+        <span className="right" style={{ fontSize: '1.8rem' }}>
+          {price} $
+        </span>
       </div>
     </div>
   );
