c4009cf069022de006fc633470edb47ea01a1d62
Alekssey Demchenko <ad@pesochin.net>
2023-06-13 19:20:44 +0300

    Creating a Food project, Page Search

--- food-project/src/pages/Home.jsx
+++ food-project/src/pages/Home.jsx
@@ -1,16 +1,47 @@
 import React, { useEffect, useState } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { getAllCategories } from '../api';
 import Preloader from '../components/Preloader';
 import CategoryList from '../components/CategoryList';
+import Search from '../components/Search';
 
 function Home() {
   const [catalog, setCatalog] = useState([]);
+  const [filteredCatalog, setFilteredCatalog] = useState([]);
+
+  const { pathname, search } = useLocation();
+  // const { push } = useNavigate();
+
+  const handleSearch = (str) => {
+    setFilteredCatalog(
+      catalog.filter((item) => item.strCategory.toLowerCase().includes(str.toLowerCase()))
+    );
+    // push({
+    //   pathname,
+    //   search: `?search=${str}`,
+    // });
+  };
 
   useEffect(() => {
-    getAllCategories().then((data) => setCatalog(data.categories));
+    getAllCategories().then((data) => {
+      setCatalog(data.categories);
+      setFilteredCatalog(
+        search
+          ? data.categories.filter((item) =>
+              item.strCategory.toLowerCase().includes(search.toLowerCase())
+            )
+          : data.categories
+      );
+    });
+    // eslint-disable-next-line
   }, []);
 
-  return <>{!catalog.length ? <Preloader /> : <CategoryList catalog={catalog} />}</>;
+  return (
+    <>
+      <Search cb={handleSearch} />
+      {!catalog.length ? <Preloader /> : <CategoryList catalog={filteredCatalog} />}
+    </>
+  );
 }
 
 export default Home;
