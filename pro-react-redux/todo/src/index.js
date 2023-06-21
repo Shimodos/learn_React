51fd0a85eb902b4bed6daf345699d23faa9caf57
Alekssey Demchenko <ad@pesochin.net>
2023-06-21 14:45:48 +0300

    pro-react-redux: Start ToDo App: React_elements

--- pro-react-redux/todo/src/index.js
+++ pro-react-redux/todo/src/index.js
@@ -1,1 +1,14 @@
-console.log('Hello World!');
+import React from "react";
+import ReactDOM from "react-dom";
+
+const el =(
+    <div>
+        <h1>My ToDo </h1>
+        <input placeholder="search" />
+        <ul>
+            <li>Learn React</li>
+            <li>Build Awesome App</li>
+        </ul>
+    </div>
+);
+ReactDOM.render(el, document.getElementById("root"));
