f423418f100cf177953c8c30e3c3f594318477fa
Alekssey Demchenko <ad@pesochin.net>
2023-06-27 08:58:40 +0300

    pro-react-redux: Start ToDo App: delet Element

--- pro-react-redux/todo/src/App.js
+++ pro-react-redux/todo/src/App.js
@@ -1,3 +1,5 @@
+import React, { Component } from 'react';
+
 import ToDoList from './components/todo-list/todo-list';
 import AppHeader from './components/app-header/app-header';
 import SearchPanel from './components/search-panel/search-panel';
@@ -5,23 +7,37 @@ import ItemStatusFilter from './components/item-status-filter/item-status-filter
 
 import './index.css';
 
-const App = () => {
-  const todoData = [
-    { label: 'Drink Coffee', important: false, id: 1 },
-    { label: 'Build React App', important: true, id: 2 },
-    { label: 'Have a lunch', important: false, id: 3 },
-  ];
+export default class App extends Component {
+  state = {
+    todoData: [
+      { label: 'Drink Coffee', important: false, id: 1 },
+      { label: 'Build React App', important: true, id: 2 },
+      { label: 'Have a lunch', important: false, id: 3 },
+    ],
+  };
 
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
+  deleteItem = (id) => {
+    this.setState(({ todoData }) => {
+      const idx = todoData.findIndex((el) => el.id === id);
+
+      const newArray = [...todoData.slice(0, idx), ...todoData.slice(idx + 1)];
+
+      return {
+        todoData: newArray,
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div className="todo-app">
+        <AppHeader toDo={1} done={3} />
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+        <ToDoList todos={this.state.todoData} onDeleted={this.deleteItem} />
       </div>
-      <ToDoList todos={todoData} onDeleted={(id) => console.log('del', id)} />
-    </div>
-  );
-};
-
-export default App;
+    );
+  }
+}
