55a364f310b80458cbf2fdc0989979c13ad8b458
Alekssey Demchenko <ad@pesochin.net>
2023-07-01 14:12:28 +0300

    pro-react-redux: Start StarDB: API finish

--- pro-react-redux/todo/src/components/item-status-filter/item-status-filter.js
+++ pro-react-redux/todo/src/components/item-status-filter/item-status-filter.js
@@ -1,32 +1,65 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-// import './item-status-filter.css';
-
-export default class ItemStatusFilter extends Component {
-  buttons = [
+const ItemStatusFilter = ({ filter, onFilterChange }) => {
+  const buttons = [
     { name: 'all', label: 'All' },
     { name: 'active', label: 'Active' },
     { name: 'done', label: 'Done' },
   ];
 
-  render() {
-    const { filter, onFilterChange } = this.props;
+  return (
+    <div className="btn-group">
+      {buttons.map(({ name, label }) => {
+        const isActive = filter === name;
+        const clazz = isActive ? 'btn-primary' : 'btn-outline-secondary';
+        return (
+          <button
+            type="button"
+            className={`btn ${clazz}`}
+            key={name}
+            onClick={() => onFilterChange(name)}
+          >
+            {label}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
 
-    const buttons = this.buttons.map(({ name, label }) => {
-      const isActive = filter === name;
-      const clazz = isActive ? 'btn-primary' : 'btn-outline-secondary';
-      return (
-        <button
-          type="button"
-          className={`btn ${clazz}`}
-          key={name}
-          onClick={() => onFilterChange(name)}
-        >
-          {label}
-        </button>
-      );
-    });
+export default ItemStatusFilter;
 
-    return <div className="btn-group">{buttons}</div>;
-  }
-}
+// Class components version:
+
+// import React, { Component } from 'react';
+//
+// // import './item-status-filter.css';
+//
+// export default class ItemStatusFilter extends Component {
+//   buttons = [
+//     { name: 'all', label: 'All' },
+//     { name: 'active', label: 'Active' },
+//     { name: 'done', label: 'Done' },
+//   ];
+//
+//   render() {
+//     const { filter, onFilterChange } = this.props;
+//
+//     const buttons = this.buttons.map(({ name, label }) => {
+//       const isActive = filter === name;
+//       const clazz = isActive ? 'btn-primary' : 'btn-outline-secondary';
+//       return (
+//         <button
+//           type="button"
+//           className={`btn ${clazz}`}
+//           key={name}
+//           onClick={() => onFilterChange(name)}
+//         >
+//           {label}
+//         </button>
+//       );
+//     });
+//
+//     return <div className="btn-group">{buttons}</div>;
+//   }
+// }
