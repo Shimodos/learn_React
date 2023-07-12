f4e99645718d0a9527619bd06bd64a7f5622e3cd
Alekssey Demchenko <ad@pesochin.net>
2023-07-12 20:56:44 +0300

    pro-react-redux: Start StarDB: add Pages, Router_01

--- pro-react-redux/star-db/src/App.js
+++ pro-react-redux/star-db/src/App.js
@@ -10,45 +10,64 @@ import { SwapiServiceProvider } from './components/swapi-serwice-contex/swapi-se
 import PeoplePage from './components/pages/people-page';
 import PlanetPage from './components/pages/planet-page';
 import StarshipPage from './components/pages/starship-page';
+import StarshipDetails from './components/sw-components/starship-ditails';
 
 import './app.scss';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 export default class App extends Component {
   swapiService = new SwapiService();
 
-  state = {
-    // showRandomPlanet: true,
-  };
-
-  // toggleRandomPlanet = () => {
-  //   this.setState((state) => {
-  //     return {
-  //       showRandomPlanet: !state.showRandomPlanet,
-  //     };
-  //   });
-  // };
-
   render() {
-    // const planet = this.state.showRandomPlanet ? <RandomPlanet /> : null;
-
     return (
       <ErrorBoundary>
         <SwapiServiceProvider value={this.swapiService}>
-          <div className="stardb-app">
-            <Header />
-            <RandomPlanet />
+          <Router>
+            <div className="stardb-app">
+              <Header />
+              <RandomPlanet />
 
-            <PeoplePage />
-            <PlanetPage />
-            <StarshipPage />
-
-            {/*<button*/}
-            {/*  className="toggle-planet btn btn-warning btn-lg"*/}
-            {/*  onClick={this.toggleRandomPlanet}*/}
-            {/*>*/}
-            {/*  Toggle Random Planet*/}
-            {/*</button>*/}
-          </div>
+              <Routes>
+                <Route path={'/'} element={<h2>Welcome to StarDB</h2>} />
+                <Route
+                  path={'/people'}
+                  element={
+                    <>
+                      <h2>People</h2>
+                      <PeoplePage />
+                    </>
+                  }
+                />
+                <Route
+                  path={'/planets'}
+                  element={
+                    <>
+                      <h2>Planets</h2>
+                      <PlanetPage />
+                    </>
+                  }
+                />
+                <Route
+                  path={'/starships'}
+                  element={
+                    <>
+                      <h2>Starships</h2>
+                      <StarshipPage />
+                    </>
+                  }
+                />
+                <Route
+                  path={'/starships/:id'}
+                  element={<StarshipDetails />}
+                  render={({ match }) => {
+                    const { id } = match.params;
+                    return <StarshipDetails itemId={id} />;
+                  }}
+                />
+              </Routes>
+            </div>
+          </Router>
         </SwapiServiceProvider>
       </ErrorBoundary>
     );
--- pro-react-redux/star-db/src/components/pages/starship-page.js
+++ pro-react-redux/star-db/src/components/pages/starship-page.js
@@ -1,28 +1,45 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+//
+// import StarshipDetails from '../sw-components/starship-ditails';
+// import { StarshipList } from '../sw-components/item-list';
+// import Row from '../row/row';
+//
+// class StarshipPage extends Component {
+//   state = {
+//     selectedPerson: null,
+//   };
+//
+//   onItemSelected = (selectedItem) => {
+//     this.setState({ selectedItem });
+//   };
+//
+//   render() {
+//     const { selectedItem } = this.state;
+//
+//     return (
+//       <Row
+//         left={<StarshipList onItemSelected={this.onItemSelected} />}
+//         right={<StarshipDetails itemId={selectedItem} />}
+//       />
+//     );
+//   }
+// }
+//
+// export default StarshipPage;
 
-import StarshipDetails from '../sw-components/starship-ditails';
+import React from 'react';
+
 import { StarshipList } from '../sw-components/item-list';
-import Row from '../row/row';
+import {} from 'react-router-dom';
 
-class StarshipPage extends Component {
-  state = {
-    selectedPerson: null,
-  };
-
-  onItemSelected = (selectedItem) => {
-    this.setState({ selectedItem });
-  };
-
-  render() {
-    const { selectedItem } = this.state;
-
-    return (
-      <Row
-        left={<StarshipList onItemSelected={this.onItemSelected} />}
-        right={<StarshipDetails itemId={selectedItem} />}
-      />
-    );
-  }
-}
+const StarshipPage = () => {
+  return (
+    <StarshipList
+      onItemSelected={(itemId) => {
+        const newPath = `/starships/${itemId}`;
+      }}
+    />
+  );
+};
 
 export default StarshipPage;
