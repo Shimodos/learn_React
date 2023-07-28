e2582252fd9f5efe3f2a388b1beab3f20c82876a
Alekssey Demchenko <ad@pesochin.net>
2023-07-29 00:45:43 +0300

    pro-react-redux: WebPack 002

--- pro-react-redux/build-sandbox/src/index.js
+++ pro-react-redux/build-sandbox/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './main.scss';
 
-const App = () => <p>Hello World</p>;
+const App = () => <h1>This is WebPac React App</h1>;
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- pro-react-redux/build-sandbox/webpack.config.js
+++ pro-react-redux/build-sandbox/webpack.config.js
@@ -1,40 +1,95 @@
-module.exports = {
-  mode: 'development',
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [{ loader: 'babel-loader' }],
-      },
+module.exports = (env = {}) => {
+  const { mode = 'development' } = env;
 
-      // Loaders image
-      {
-        test: /\.(png|jpg|jpeg|gif|ico)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              outputPath: 'images',
-              name: '[name]-[sha1:hash:7].[ext]',
+  const isProd = mode === 'production';
+  const isDev = mode === 'development';
+
+  const getStyleLoaders = () => {
+    return [isProd ? MiniCssExtractPlugin.loader : 'style-loader', 'css-loader'];
+  };
+
+  const getPlugins = () => {
+    const plugins = [
+      new HtmlWebpackPlugin({
+        title: 'Hello World',
+        buildTime: new Date().toISOString(),
+        template: 'public/index.html',
+      }),
+    ];
+    if (isProd) {
+      plugins.push(
+        new MiniCssExtractPlugin({
+          filename: 'main-[hash:8].css',
+        })
+      );
+    }
+    return plugins;
+  };
+
+  return {
+    mode: isProd ? 'production' : isDev && 'development',
+
+    output: {
+      filename: isProd ? 'main-[hash:8].js' : undefined,
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+        },
+
+        // Loaders image
+        {
+          test: /\.(png|jpg|jpeg|gif|ico)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                outputPath: 'images',
+                name: '[name]-[sha1:hash:7].[ext]',
+              },
             },
-          },
-        ],
-      },
-      // Loaders fonts
-      {
-        test: /\.(ttf|otf|eot|woff|woff2)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              outputPath: 'fonts',
-              name: '[name].[ext]',
+          ],
+        },
+
+        // Loaders fonts
+        {
+          test: /\.(ttf|otf|eot|woff|woff2)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                outputPath: 'fonts',
+                name: '[name].[ext]',
+              },
             },
-          },
-        ],
-      },
-    ],
-  },
+          ],
+        },
+
+        // Loaders css style
+        {
+          test: /\.(css)$/,
+          use: getStyleLoaders(),
+        },
+
+        // Loaders scss/sass
+        {
+          test: /\.(s[ca]ss)$/,
+          use: [...getStyleLoaders(), 'sass-loader'],
+        },
+      ],
+    },
+
+    plugins: getPlugins(),
+
+    devServer: {
+      open: true,
+    },
+  };
 };
