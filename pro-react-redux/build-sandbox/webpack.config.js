6608b40688fe85b2d2790e949ff94e892f195b75
Alekssey Demchenko <ad@pesochin.net>
2023-07-28 21:17:23 +0300

    pro-react-redux: WebPack 001

---
+++ pro-react-redux/build-sandbox/webpack.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  mode: 'development',
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [{ loader: 'babel-loader' }],
+      },
+
+      // Loaders image
+      {
+        test: /\.(png|jpg|jpeg|gif|ico)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'images',
+              name: '[name]-[sha1:hash:7].[ext]',
+            },
+          },
+        ],
+      },
+      // Loaders fonts
+      {
+        test: /\.(ttf|otf|eot|woff|woff2)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'fonts',
+              name: '[name].[ext]',
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
